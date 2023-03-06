043e3cdc49b28068d6784ca496615b41424879dc
gekoh <kohlweiss@gmail.com>
2023-03-06 14:40:35 +0100

    adding descending counter of entity materialization, entities with same primary key can appear multiple times in history, new view column counts the occurrence but reverse back into history

--- lib/yagen-generator-lib/src/main/resources/com/github/gekoh/yagen/ddl/LatestSnapshotView.vm.pl.sql
+++ lib/yagen-generator-lib/src/main/resources/com/github/gekoh/yagen/ddl/LatestSnapshotView.vm.pl.sql
@@ -1,5 +1,11 @@
 create or replace view ${objectName} as
-select *
+select h.*,
+#if( $is_oracle )
+       row_number() over (partition by #foreach( $pkColumn in $pkColumns )#if($foreach.count > 1), #{end}${pkColumn}#end order by TRANSACTION_TIMESTAMP desc)
+#else
+       (select count(*)+1 from ${hstTableName} where #foreach( $pkColumn in $pkColumns )${pkColumn}=h.${pkColumn} and #end TRANSACTION_TIMESTAMP>h.TRANSACTION_TIMESTAMP and OPERATION='I')
+#end
+        as rev_materialization_nr
 from ${hstTableName} h
 where (INVALIDATED_AT is null and OPERATION<>'D')
    or exists (
