45ccdbc735bbd259cc6155b480a1223e45917656
gekoh <kohlweiss@gmail.com>
2023-06-02 08:04:40 +0200

    tuning performance of latest snapshot views

--- lib/yagen-generator-lib/src/main/resources/com/github/gekoh/yagen/ddl/LatestSnapshotView.vm.pl.sql
+++ lib/yagen-generator-lib/src/main/resources/com/github/gekoh/yagen/ddl/LatestSnapshotView.vm.pl.sql
@@ -1,24 +1,21 @@
 create or replace view ${objectName} as
 select h.*,
 #if( $is_oracle )
-       row_number() over (partition by #foreach( $pkColumn in $pkColumns )#if($foreach.count > 1), #{end}${pkColumn}#end order by TRANSACTION_TIMESTAMP desc)
+       row_number() over (partition by #foreach( $pkColumn in $pkColumns )#if($foreach.count > 1), #{end}h.${pkColumn}#end order by h.TRANSACTION_TIMESTAMP desc)
 #else
        (select count(*)+1 from ${hstTableName} where #foreach( $pkColumn in $pkColumns )${pkColumn}=h.${pkColumn} and #end TRANSACTION_TIMESTAMP>h.TRANSACTION_TIMESTAMP and OPERATION='I')
 #end
         as rev_materialization_nr
 from ${hstTableName} h
-where (INVALIDATED_AT is null and OPERATION<>'D')
-   or exists (
-        select 1
-        from ${hstTableName}
-        where
+    left join ${hstTableName} hd on
 #foreach( $pkColumn in $pkColumns )
 #if( $!{columnMap.get($pkColumn).nullable} )
-              ((${pkColumn} is null and h.${pkColumn} is null) or ${pkColumn}=h.${pkColumn}) and
+              ((hd.${pkColumn} is null and h.${pkColumn} is null) or hd.${pkColumn}=h.${pkColumn}) and
 #else
-              ${pkColumn}=h.${pkColumn} and
+              hd.${pkColumn}=h.${pkColumn} and
 #end
 #end
-          ${histColName}=h.INVALIDATED_AT and OPERATION='D'
-    )
+          hd.${histColName}=h.INVALIDATED_AT and hd.OPERATION='D'
+where (h.INVALIDATED_AT is null and h.OPERATION<>'D')
+   or hd.hst_uuid is not null
 ;
